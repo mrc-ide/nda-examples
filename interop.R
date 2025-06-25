e3768cb08760310761dde19a69ec9538d47bd6a4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-06-25 13:56:44 +0100

    Be less global

--- interop.R
+++ interop.R
@@ -1,5 +1,6 @@
-Sys.setenv("PKG_CXXFLAGS" = sprintf("-I%s", getwd()))
-cpp11::cpp_source("interop.cpp", cxx_std = "CXX17")
+withr::with_envvar(
+  c(PKG_CXXFLAGS = sprintf("-I%s", getwd())),
+  cpp11::cpp_source("interop.cpp", cxx_std = "CXX17"))
 
 # This surprises me, because we've used the column-major data provided by R
 m <- matrix(as.numeric(1:12), 3, 4)
