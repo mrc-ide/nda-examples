4cb0cbc3792a7bba1e051af87ec79696627b941c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-06-25 14:22:35 +0100

    Update comment

--- interop.R
+++ interop.R
@@ -2,7 +2,7 @@ withr::with_envvar(
   c(PKG_CXXFLAGS = sprintf("-I%s", getwd())),
   cpp11::cpp_source("interop.cpp", cxx_std = "CXX17"))
 
-# This surprises me, because we've used the column-major data provided by R
+# Data storage is organised basically the same as R here:
 m <- matrix(as.numeric(1:12), 3, 4)
 show_matrix(m)
 
